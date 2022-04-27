d930337efe154f19b9fc785ade4f6dbaef9f522a
mizantrop1973 <102754869+mizantrop1973@users.noreply.github.com>
2022-04-27 21:36:15 +0300

    Квадраты первых 20 чисел

--- Hello World/Programm.cpp
+++ Hello World/Programm.cpp
@@ -5,31 +5,11 @@
 int main()
 {
 	setlocale(LC_ALL, "Russian");
-	double a, b, c;
-	//a*x*x + b*x + c*x = 0
-	//a = 1.; b = -3.; c = 2.;
-
-	printf ("Введите коэффициенты урвнения:\n");
-	printf("Kоэффициент a = ");
-	scanf_s("%lf", &a);
-	printf("Kоэффициент b = ");
-	scanf_s("%lf", &b);
-	printf("Kоэффициент c = ");
-	scanf_s("%lf", &c);
-
-	double d = b * b - 4. * a * c;
-
-	if (d < 0.)
+	int n = 20;
+	int k = 1;
+	for (k=1; k<=20; k=k+1)
 	{
-		
-		printf("Нет решения\n");
-	}
-	else
-	{
-		d = sqrt(d);
-		double x1 = (-b + d) / (2. * a);
-		double x2 = (-b - d) / (2. * a);
-		printf("x1 = %lf x2 = %lf\n", x1, x2);
+		printf("%d  %d\n", k, k * k);
 	}
 	return 0;
 }
