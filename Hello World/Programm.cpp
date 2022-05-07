3811c4c6767e636ab27f31111af896127dec70ff
mizantrop1973 <102754869+mizantrop1973@users.noreply.github.com>
2022-05-08 00:23:53 +0300

    НОД рекурсивно

--- Hello World/Programm.cpp
+++ Hello World/Programm.cpp
@@ -2,22 +2,33 @@
 #include <locale.h>
 #include <math.h> 
 
-//НОД двух целых чисел
+//НОД двух целых чисел РЕКУРСИВНО
 
 
 int main()
 
 {
-	int gcd(int m, int n)
-	{
-		while (n != 0)
+	
+	
+	return 0;
+}
+
+int gcd1(int m, int n)
+{
+	if (n == 0)
+		return m;
+	int r = m % n;
+	// НОД (m , n) = НОД (n ,r)
+	return gcd1(n, r);
+
+}
+
+
+		/* while (n != 0)
 		{
 			int r = m % n;
 			m = n; n = r;
 		}
-		//Утверждение n=0 и НОД (m,n)=НОД(m0,n0)
-		return m;
-	}
+		Утверждение n=0 и НОД (m,n)=НОД(m0,n0)
+		return m;*/
 	
-	return 0;
-}
