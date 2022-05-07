a99a659ded2c16bb598c9fbb004f51cb02be8cbf
mizantrop1973 <102754869+mizantrop1973@users.noreply.github.com>
2022-05-08 01:22:15 +0300

    3 метода расчета НОД

--- Hello World/Programm.cpp
+++ Hello World/Programm.cpp
@@ -1,18 +1,46 @@
 #include <stdio.h>
+
 #include <locale.h>
 #include <math.h> 
 
-//НОД двух целых чисел РЕКУРСИВНО
+//НОД расширенный алгоритм Евклида
+int gcd(int m, int n);
+int gcd1(int m, int n);
+int extgcd(int m, int n, int* u, int* v);
 
 
 int main()
 
 {
-	
-	
+	setlocale(LC_ALL, "Russian");
+	int m, n;
+	printf("Введите 2 числа:\n");
+	scanf_s("%d%d", &m, &n);
+	int d = gcd(m, n);
+	printf("НОД=%d\n", d);
+
+	d = gcd1(m, n);
+	printf("НОД1=%d\n", d);
+
+	int u, v;
+	d = extgcd(m, n, &u, &v);
+	printf("НОД2=%d  u=%d  v=%d\n", d, u, v);
 	return 0;
 }
 
+int gcd(int m, int n)
+{
+	int a = m;
+	int b = n;
+
+	while (b != 0)
+	{
+		int r = a % b;
+		a = b; b = r;
+	}
+	//Утверждение n=0 и НОД (m,n)=НОД(m0,n0)
+	return a;
+}
 int gcd1(int m, int n)
 {
 	if (n == 0)
@@ -20,15 +48,24 @@ int gcd1(int m, int n)
 	int r = m % n;
 	// НОД (m , n) = НОД (n ,r)
 	return gcd1(n, r);
+}
+int extgcd(int m, int n, int* u, int* v)
+{
+	if (n == 0)
+	{
+		*u = 1; *v = 0;
+		return m;
+	}
+	int q = m / n; int r = m % n;
+	int u1, v1;
+	int d = extgcd(n, r, &u1, &v1);
+	*u = v1; *v = u1 - v1 * q;
+
+
+	//НОД (m , n) = НОД (n ,r)
+	return d;
 
 }
 
 
-		/* while (n != 0)
-		{
-			int r = m % n;
-			m = n; n = r;
-		}
-		Утверждение n=0 и НОД (m,n)=НОД(m0,n0)
-		return m;*/
-	
+
