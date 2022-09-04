3ef1b4ca2f48ef7bb7601957b21958a6d98709ea
mizantrop1973 <102754869+mizantrop1973@users.noreply.github.com>
2022-09-04 21:19:33 +0300

    нод полный

--- Hello World/Programm.cpp
+++ Hello World/Programm.cpp
@@ -2,22 +2,32 @@
 #include <locale.h>
 #include <math.h> 
 
-//НОД двух целых чисел
 
+
+//НОД двух целых чисел
+int gcd(int m, int n);
+int m, n;
 
 int main()
-
 {
-	int gcd(int m, int n)
-	{
-		while (n != 0)
-		{
-			int r = m % n;
-			m = n; n = r;
-		}
-		//Утверждение n=0 и НОД (m,n)=НОД(m0,n0)
-		return m;
-	}
-	
+	setlocale(LC_ALL, "Russian");
+	printf("Введите m:\n");
+	scanf_s("%d", &m);
+	printf("Введите n:\n");
+	scanf_s("%d", &n);
+	m=gcd(m, n);
+	printf(" НОД m и n = %d\n ", m);
 	return 0;
+
 }
+int gcd(int m, int n)
+{
+	while (n != 0)
+	{
+		int r = m % n;
+		m = n; n = r;
+	}
+	//Утверждение n=0 и НОД (m,n)=НОД(m0,n0)
+	return m;
+}
+
