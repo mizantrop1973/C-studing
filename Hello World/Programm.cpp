6cce603b4b5dc1f2d5cca7e75b8b2316da667ddb
mizantrop1973 <102754869+mizantrop1973@users.noreply.github.com>
2022-09-06 22:29:17 +0300

    NOD_recurse

--- Hello World/Programm.cpp
+++ Hello World/Programm.cpp
@@ -5,7 +5,7 @@
 
 
 //НОД двух целых чисел
-int gcd(int m, int n);
+int gcd1(int m, int n);
 int m, n;
 
 int main()
@@ -15,19 +15,23 @@ int main()
 	scanf_s("%d", &m);
 	printf("Введите n:\n");
 	scanf_s("%d", &n);
-	m=gcd(m, n);
+	m=gcd1(m, n);
 	printf(" НОД m и n = %d\n ", m);
 	return 0;
 
 }
-int gcd(int m, int n)
+int gcd1(int m, int n)
 {
-	while (n != 0)
+	if (n == 0)
+		return m;
+	int r = m % n;
+	return gcd1(n, r);
+	/*while (n != 0)
 	{
 		int r = m % n;
 		m = n; n = r;
 	}
 	//Утверждение n=0 и НОД (m,n)=НОД(m0,n0)
-	return m;
+	return m;*/
 }
 
