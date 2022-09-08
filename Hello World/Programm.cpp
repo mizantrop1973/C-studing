f60b894d02b848396f5ca31efaa6cbc70865966d
mizantrop1973 <102754869+mizantrop1973@users.noreply.github.com>
2022-09-09 02:47:16 +0300

    Индуктивные_функции

--- Hello World/Programm.cpp
+++ Hello World/Programm.cpp
@@ -2,18 +2,42 @@
 #include <locale.h>
 #include <math.h> 
 
+double sum(FILE* f);
+
 int main()
+
 {
 	setlocale(LC_ALL, "Russian");
-	int n, k;
-	//int k = 1;
-	printf("Введите начальное число k=");
-	scanf_s("%d", &k);
-	printf("Введите конечное число n=");
-	scanf_s("%d", &n);
-	for (k=k; k<=n; k++)
+	FILE* in, * out;
+	fopen_s (&in, "c:/Users/Дмитрий/Documents/input.txt", "r");
+	if (in == NULL)
 	{
-		printf("%d  %d\n", k, k * k);
+		perror("Не удалось прочесть файл\n");
+		return(-1);
 	}
+	double s = sum(in);
+	fclose(in);
+
+	fopen_s(&out, "c:/Users/Дмитрий/Documents/output.txt", "w");
+	if (out == NULL)
+	{
+		perror("Не удалось прочесть файл\n");
+		return(-1);
+	}
+	fprintf_s(out, "% lf\n", s);
+	fclose(out);
 	return 0;
 }
+double sum (FILE* f)
+{
+	double s = 0.;
+	double a;
+	while (fscanf_s(f, "%lf", &a) == 1)
+	{
+		s += a;
+	}
+	return s;
+	
+}
+
+		
