6ed78bfd6281480b989e04ad5eadd968bba12a33
mizantrop1973 <102754869+mizantrop1973@users.noreply.github.com>
2022-09-13 02:40:11 +0300

    Вавилонский корень (Итерации)

--- Hello World/Programm.cpp
+++ Hello World/Programm.cpp
@@ -2,27 +2,47 @@
 #include <stdio.h>
 #include <locale.h>
 #include <math.h> 
+#include <cassert>
 
+double squareRoot(double a, double eps);
 
 int main()
 
 {
 	setlocale(LC_ALL, "Russian");
-	printf("Значение среднеарифметического последовательности \n");
-	double a, p = 0, s = 0.;
-	int l = 0;
-
-	printf("Введите коэф-ты многочлена (через пробел, по убыванию). Конец ввода обозначте решёткой << # >> и нажмите ENTER : \n");
-	//при вводе с клавиатуры для лклнчания ввода использовать символ "#";
-	while (scanf("%lf", &a) == 1)
+	printf(" ВАВИЛОНСКОЕ ВЫЧИСЛЕНИЕ КОРНЯ ЧИСЛА С ЗАДАННОЙ ТОЧНОСТЬЮ");
+	double a, r, eps;
+	while (true)
 	{
-		s = s + a;
-		l = l + 1;
-		p = s / l;
+		printf("\n Введите a:  ");
+		scanf("%lf", &a);
+			if(a<0)
+			break;
+
+		printf(" Введите eps:  ");
+		scanf("%lf", &eps);
+				
+		r = squareRoot(a, eps);
+		printf("\n r = %lf\n", r);
+		printf("\n Проверка 1: sqrt(a) = %lf\n", sqrt(a));
+		printf(" Проверка 2: r * r = %lf\n", r * r);
+		printf("\n НОВЫЙ РАСЧЕТ\n");
 	}
-	printf("Значение среднеарифметического последовательности p = %lf\n", p);
 
 	return 0;
+
+}
+double squareRoot(double a, double eps)
+{
+	assert (a >= 0.); // Добавили инклуд вначале #include <cassert>
+	double x = a;
+	while (fabs(x * x - a) > eps)
+	{
+		x = (x + a / x) / 2.;
+	}
+	fflush(stdin);
+	return x;
+
 }
 
 		
