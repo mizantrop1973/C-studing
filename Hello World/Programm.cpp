7e4de4c629d8b9d5849abfadf4528e25a4930ca5
mizantrop1973 <102754869+mizantrop1973@users.noreply.github.com>
2022-09-13 20:38:02 +0300

    Расщиренный алгоритм Евклида (НОД) ИНВАРИАНТ

--- Hello World/Programm.cpp
+++ Hello World/Programm.cpp
@@ -4,58 +4,59 @@
 #include <math.h> 
 #include <cassert>
 
-double  degree(double a, int n);
+//НОД расширенный алгоритм Евклида ИНВАРИАНТ
+
+int extgcd(int m, int n, int* u, int* v);
 
 int main()
 
 {
+	int m, n;
 	setlocale(LC_ALL, "Russian");
-	printf(" АЛГОРИТМ БЫСТРОГО ВОЗВЕДЕНИЯ В СТЕПЕНЬ (ИНВАРИАНТ)");
-	double  a;
-	int n;
+	printf(" РАСШИРЕННЫЙ АЛГОРИТМ ЕВКЛИДА (ИНВАРИАНТ)\n\n");
+	
 	while (true)
 	{
-		printf("\n Введите возводимое в степень число a:  ");
-		scanf("%lf", &a);
-
-		printf(" Введите степень n:  ");
-		scanf("%d", &n);
-		if (n < 0)
-			break;
-		double x = degree(a, n);
-		printf("\n x = %lf\n", x);
 		
+		printf("\n Введите 2 числа m и n : \n");
+		printf("\n m = ");
+		scanf_s("%d", &m);
+		printf("\n n = ");
+		scanf_s("%d", &n);
+		
+		int u, v;
+		int d = extgcd(m, n, &u, &v);
+		printf("\n НОД = %d  u = %d  v = %d\n\n\n", d, u, v);
+				
 		printf("\n НОВЫЙ РАСЧЕТ\n");
 	}
 
 	return 0;
 
 }
-double degree(double a, int n)
-{
-	assert(n >= 0.); // Добавили инклуд вначале #include <cassert>
-	double p = 1;
-	double b = a;
-	int k = n;
-	// утверждение b ^ k  * p = a;
 
-	while (k > 0)
+int extgcd(int m, int n, int* u, int* v)
+{
+	int a = m;
+	int b = n;
+	int u1 = 1; int v1 = 0; 
+	int u2 = 0; int v2 = 1;
+	assert(a == u1 * m + v1 * n && b == u2 * m + v2 * n);
+	//Утверждение 1 НОД(a, b) = НОД(m, n);
+    //Утверждение 2 int a = u1 * m + v1 * n;
+	//Утверждение 3 int b = u2 * m + v2 * n;
+	while (b != 0)
 	{
-		if (k % 2 == 0)
-		{
-			k = k / 2;
-			b = b * b;
-		}
-		else
-		{
-			k = k - 1;
-			p = p * b;
-		}
+		int q = a / b; // в Си операция с целыми числами приводит к целой части htpekmnfnf
+		int r = a % b;
+		a = b; b = r;
+		int tmp = u2; u2 = u1 - q * u2;
+		u1 = tmp;
+		tmp = v2;v2 = v1 - q * v2;
+		v1 = tmp;
 	}
-	
-	//fflush(stdin);
-	return p;
+	*u = u1; *v = v1;
+
+	return a;
 
 }
-
-		
