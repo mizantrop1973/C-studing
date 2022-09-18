6ff4224d19a9ab493c0a67ea6dce82a752c3fcc0
mizantrop1973 <102754869+mizantrop1973@users.noreply.github.com>
2022-09-18 20:34:02 +0300

    Программа сортировки массива (пузырьковая)

--- Hello World/Programm.cpp
+++ Hello World/Programm.cpp
@@ -1,95 +1,106 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
+#include <stdlib.h>
 #include <locale.h>
 #include <math.h> 
 #include <cassert>
 
-//Организация последоватенльного поиска в массиве ИНВАРИАНТ
+//Сортировка ИНВАРИАНТ
 
-int secSearch(double *a, int n, double x);
-const int N = 10000;
-int n; 
-double x;
-double a[N];
-const double EPS = 0.0000001;
+void swap(double* x, double* y);
+void bubbleSort(double* a, int n);
+
 
 int main()
-
 {
-	
 	setlocale(LC_ALL, "Russian");
-	printf(" Организация последоватенльного поиска в массиве\n\n");
-	
+	int n, i;
+	double* a = NULL;
 	while (true)
 	{
-		printf("\n Введите  длину массива n < N = %d :  ", N);
-		scanf_s("%d", &n);
-		
-		while (n > N || n <= 0)
-		{
-			printf("\n Длина массива менее 1(единицы) или выше допустимого (%d). Попробуйте еще раз\n", N);
-			scanf_s("%d", &n);
-		}
-		printf("\n Введите масcив a длиной %d вещественных чисел (После каждого нажимать ENTER)  :\n", n);
-		int i = 0;
-				for (int i=0; i <= n - 1; i++)
-		{
-			printf(" a[%d] = ", i);
+		printf("\n ПРОГРАММА СОРТИРОВКИ ЭЛЕМЕНТОВ МАССИВА ПО ВОЗРАСТАНИЮ \n\n");
 
-			scanf_s("%lf", &a[i]);
+		printf("\n Введите  длину массива n :  ");
+		if (scanf("%d", &n) < 1 || n <= 0)
+			break;
+		delete[] a;
+		a = new double[n];
+
+		for (i = 0; i < n; ++i)
+		{
+			a[i] = (double)(rand() % 100);
 		}
 
-		printf("\n Введен массив  :");
-		for (i = 0; i <= n - 1; ++i)
+		printf("\n Имеется массив  :\n");
+		for (i = 0; i < n; ++i)
 		{
-			printf("  %lf  ", a[i]); // пробел в формате печати обязателен
-			//getchar(); getchar();
+			printf("  %6.1lf  ", a[i]);
+			if ((i + 1) % 10 == 0)
+				printf("\n");
 		}
-		printf("\n\n Введите элемент поиска x = ");
-		scanf_s("%lf", &x);
+		printf("\n=================================\n");
 
-		int f = secSearch(a,  n,  x);// сделал все в функции main, так как с отдельной функцией возникли проблемы чтения переменной idx
-		/*		bool found = false;
+		bubbleSort(a, n);
+		printf(" Отсортированный массив :  \n");
 
-		for (int i = 0; !found && i <= (n - 1); ++i)
+		for (i = 0; i < n; ++i)
 		{
-			if (fabs(a[i] - x) < EPS && a[i] * x >= 0)
-			{
-				found = true;
-				idx = &i;
-			}
-		}*/
-
-		if (f >= 0)
-		{
-			printf("\n\n ОТВЕТ: Элемент х = %lf найден : Это a[%d]\n\n", x, f);
+			printf("  %6.1lf  ", a[i]);
+			if ((i + 1) % 10 == 0)
+				printf("\n");
 		}
-		else
-		{
-			printf("\n\n ОТВЕТ: Элемент х = %lf не найден. \n\n", x);
-		}
-				
-		printf("\n\n НОВЫЙ ПОИСК\n");
+		printf("\n=================================\n");
 	}
-
+	delete[] a;
 	return 0;
 
 }
 
 
-int secSearch(double* a, int n, double x)
+void swap(double* x, double* y) // алгоритм перемены местами членов массива
 {
-	int idx = -1;
-	
-	for (int i = 0; idx < 0 && i <= (n - 1); ++i)
-	{
-		if (fabs(a[i] - x) < EPS && a[i] * x >= 0)
-		{
-			idx = i;
-		}
-	}
-
-	return idx;
-
+	double tmp = *x;
+	*x = *y;
+	*y = tmp;
 }
 
+
+
+void bubbleSort(double* a, int n) //оптимизация пузырьковой сортировки (наивный)
+{
+	bool inverse = true;
+	int k = 0;   //количество проходов
+	while (inverse)
+	{
+		inverse = false;
+		for (int i = 0; i <= n - 1 - k; ++i) // каждый проход перемещает максимальный элемент массива в конец, мы его исключаем
+		{
+			if (a[i] > a[i + 1]) // количество сравнений в одном проходе n (иногда n+1)
+			{
+				swap(&(a[i]), &(a[i + 1]));
+				inverse = true;
+			}
+		}
+		++k;  //  количество проходов уменбшается, n = (n * (n-1))/2
+	}
+}
+
+void directSort(double* a, int n) //прямой выбор (наивный)
+{
+	for (int i = 0; i <= n - 1; ++i)
+	{
+		int m = i;
+		for (int j = i + 1; j < n; ++j)
+		{
+			if (a[j] < a[m])
+			{
+				m = j;
+			}
+		}
+
+		if (m != i)
+		{
+			swap(&(a[i]), &(a[m]));
+		}
+	}
+}
