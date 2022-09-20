e6b5e916b99c7d98d0bf8a23d4c08fef9560c887
mizantrop1973 <102754869+mizantrop1973@users.noreply.github.com>
2022-09-20 17:04:25 +0300

    Сортировка методом ПИРАМИДЫ

--- Hello World/Programm.cpp
+++ Hello World/Programm.cpp
@@ -8,10 +8,12 @@
 //Быстрая соритировка
 
 void swap(double* x, double* y);
-void bubbleSort(double* a, int n);
-void partition(double* a, int n, int* m);
-void quickSort(double* a, int n);
+//void bubbleSort(double* a, int n);
+//void partition(double* a, int n, int* m);
+//void quickSort(double* a, int n);
 bool binSearch(double* a, int n, double x, int* idx);
+void heapSort(double* a, int n);
+void sieve (double *a, int n, int i);
 
 int main()
 {
@@ -19,7 +21,7 @@ int main()
 	int n, i;
 	double* a = NULL;
 
-	printf("\n ПРОГРАММА СОРТИРОВКИ ЭЛЕМЕНТОВ МАССИВА ПО ВОЗРАСТАНИЮ \n\n");
+	printf("\n ПРОГРАММА СОРТИРОВКИ ЭЛЕМЕНТОВ МАССИВА МЕТОДОМ КУЧИ (ПИРАМИДЫ) \n\n");
 
 	while (true)
 	{
@@ -43,12 +45,12 @@ int main()
 		}
 		printf("\n=================================\n");
 
-		quickSort(a, n);
+		heapSort(a, n);
 		printf(" Отсортированный массив :  \n");
 
 		for (i = 0; i < n; ++i)
 		{
-			printf("  %6.1lf  ", a[i]);
+			printf("  %4.1lf  ", a[i]);
 			if ((i + 1) % 10 == 0)
 				printf("\n");
 		}
@@ -66,11 +68,11 @@ int main()
 
 			if (found)
 			{
-				printf("\n\n Элемент х = %lf найден : Это a[%d]\n\n", x, idx);
+				printf("\n\n ОТВЕТ: Элемент х = %lf найден : Это a[%d]\n\n", x, idx);
 			}
 			else
 			{
-				printf("\n\n Элемент х = %lf не найден \n\n", x);
+				printf("\n\n ОТВЕТ: Элемент х = %lf не найден \n\n", x);
 			}
 			printf("\n\n НОВЫЙ ПОИСК\n");
 		}
@@ -88,6 +90,90 @@ void swap(double* x, double* y)
 	*y = tmp;
 }
 
+
+bool binSearch(double* a, int n, double x, int* idx)
+{
+	bool found = false;
+	if (n <= 0 || x <= a[0])
+	{
+		*idx = 0;
+		return (n > 0 && x >= a[0]); // (трюк - x = a[0}
+	}
+	else if (x > a[n - 1])
+	{
+		*idx = n;
+		return false;
+	}
+	else
+	{
+		assert(n > 0 && x >= a[0] && x <= a[n - 1]);
+		int beg = 0;
+		int end = n - 1;
+		assert(a[beg] < x && x <= a[end]);
+		while (end - beg > 1)
+		{
+			int c = (end + beg) / 2;
+			assert(beg < c&& c < end);
+			if (a[c] < x)
+			{
+				beg = c;
+			}
+			else if (a[c] > x)
+			{
+				end = c;
+			}
+			else
+			{
+				// assert a[c] == x; - сравнивать вещественные числа некорректно
+				*idx = c;
+				return true;
+			}
+		}
+		assert(a[beg] < x && x <= a[end]);
+		*idx = end;
+		return (x >= a[end]);
+	}
+}
+
+void heapSort(double* a, int n)
+{
+	//I-ый этап, построение пирамиды
+	int k = n / 2;   // у  k нет детей (2k +1 == n+1 > n-1)
+	while (k > 0)
+	{
+		--k;
+		sieve(a, n, k); // Просеивание снизу справа - по окончанию будет построена ПИРАМИДА
+	}
+
+	// II-ой этап . Сортировка по пирамиде
+	k = n;
+	while(k>1)
+	{
+		--k;
+		swap(&(a[0]), &(a[k]));
+		sieve(a, k, 0);
+	}
+}
+
+void sieve(double* a, int n, int i)
+{
+	while (true)
+	{
+		int s0 = 2 * i + 1;
+		if (s0 >= n)
+			break;
+		int s1 = s0 + 1;
+		int s = s0;
+		if (s1<n && a[s1]> a[s0])
+			s = s1;
+		if (a[i] >= a[s])
+			break;
+		swap(&(a[i]), &(a[s]));
+		i = s; //переходим вниз по дереву
+	}
+}
+
+/*
 void bubbleSort(double* a, int n) //оптимизация пузырьковой сортировки (наивный)
 {
 	bool inverse = true;
@@ -141,10 +227,10 @@ void quickSort(double* a, int n)// быстрой сортировки
 		}
 		return;
 	}
-// ИНВАРИАНТ ЦИКЛА
-//Отрезки массива а(0) - a(beg - 1) и а(beg+k) - a(n-1) отсортированы;
-// beg - начало неотсортированного отрезка длинной k;
-// ИНВАРИАНТ a[beg-1] <= a[i] <= a[beg+k];
+	// ИНВАРИАНТ ЦИКЛА
+	//Отрезки массива а(0) - a(beg - 1) и а(beg+k) - a(n-1) отсортированы;
+	// beg - начало неотсортированного отрезка длинной k;
+	// ИНВАРИАНТ a[beg-1] <= a[i] <= a[beg+k];
 
 	int beg = 0;
 	int k = n;
@@ -165,7 +251,7 @@ void quickSort(double* a, int n)// быстрой сортировки
 		}
 		else // right <= left;
 		{
-			quickSort(a + beg + m +1, right);
+			quickSort(a + beg + m + 1, right);
 			// beg остается прежним
 			k -= right + 1;
 		}
@@ -182,13 +268,13 @@ void partition(double* a, int n, int* m)
 	// a[0] == x - медиана
 	// a[1], a[2],...a[i] <= x
 	// a[j]....a[n-1] >= x
-	
+
 	int i = 0; int j = n;
 
 	while (j - i > 1)
 	{
 		bool changed = false;
-		if (a[i+1]< x)
+		if (a[i + 1] <= x) // ЗДЕСЬ была ошибка, которая проявлялась при утверждении assert - был знак строго меньше.
 		{
 			++i; changed = true;
 		}
@@ -198,63 +284,18 @@ void partition(double* a, int n, int* m)
 		}
 		if (!changed)
 		{
-			assert(j - i > 1 && a[i+1] >= x && a[j-1] <= x);
+			assert(j - i > 1 && a[i + 1] > x && a[j - 1] < x); // !!!!!  в лекции было строго больше и меньше, но почему то в этом случае происходит сбой
 			++i, --j;
 			swap(&(a[i]), &(a[j]));
 		}
 	}
 	if (i > 0)
 	{
-		swap(&(a[0]), &(a[i])); 
+		swap(&(a[0]), &(a[i]));
 	}
 	*m = i;
 
 }
+*/
 
-
-
-
-bool binSearch(double* a, int n, double x, int* idx)
-{
-	bool found = false;
-	if (n <= 0 || x <= a[0])
-	{
-		*idx = 0;
-		return (n > 0 && x >= a[0]); // (трюк - x = a[0}
-	}
-	else if (x > a[n - 1])
-	{
-		*idx = n;
-		return false;
-	}
-	else
-	{
-		assert(n > 0 && x >= a[0] && x <= a[n - 1]);
-		int beg = 0;
-		int end = n - 1;
-		assert(a[beg] < x && x <= a[end]);
-		while (end - beg > 1)
-		{
-			int c = (end + beg) / 2;
-			assert(beg < c&& c < end);
-			if (a[c] < x)
-			{
-				beg = c;
-			}
-			else if (a[c] > x)
-			{
-				end = c;
-			}
-			else
-			{
-				// assert a[c] == x; - сравнивать вещественные числа некорректно
-				*idx = c;
-				return true;
-			}
-		}
-		assert(a[beg] < x && x <= a[end]);
-		*idx = end;
-		return (x >= a[end]);
-	}
-}
 
